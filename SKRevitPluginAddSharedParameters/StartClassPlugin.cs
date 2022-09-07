2fc15d761afa854a6c8da7c131d1c83b9b8a4937
Sergey Kuleshov <code4builder@gmail.com>
2022-09-07 14:32:25 +0200

    Add project files.

---
+++ SKRevitPluginAddSharedParameters/StartClassPlugin.cs
@@ -0,0 +1,13 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using SKRevitPluginPipeInsulation.Views;
+using System.Collections.Generic;
+
+namespace SKRevitPluginAddSharedParameters
+{
+    public class StartClassPlugin
+    {
+
+    }
+}
